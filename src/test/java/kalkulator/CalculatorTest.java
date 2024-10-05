df1d5233d0c6ce2b3feb2b01e4be797d584ba3b9
Wiktor Rakocki <wrakocki@inf.ug.edu.pl>
2024-10-05 12:02:23 +0200

    added one test

--- src/test/java/kalkulator/CalculatorTest.java
+++ src/test/java/kalkulator/CalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.*;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
+
 	@Test
 	public void testAddOne(){
 		// Arrange
@@ -22,5 +23,13 @@ public class CalculatorTest {
 		sut.setState(1);
 		sut.mult(2);
 		assertEquals("1*2 = 2", 2, sut.getState());
+	}
+
+	@Test
+	public void testMultOneByZero(){
+		Calculator sut = new Calculator();
+		sut.setState(1);
+		sut.mult(0);
+		assertEquals("1*0 = 0", 0, sut.getState());
 	}
 }
