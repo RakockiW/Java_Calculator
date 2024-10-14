323b1925e82bdd8f5695eec1df41bdfabf048736
WRakocki <wiktor.rakocki6@protonmail.com>
2024-10-14 21:31:14 +0200

    New operations and memory with tests

--- src/main/java/kalkulator/Calculator.java
+++ src/main/java/kalkulator/Calculator.java
@@ -2,29 +2,74 @@ package kalkulator;
 
 public class Calculator {
 	private int state = 0;
+	private int memory = 0;
 
 	public int getState() {
 		return state;
+	}
+
+	public int getMemory() {
+		return memory;
 	}
 
 	public void setState(int state) {
 		this.state = state;
 	}
 
+	public void storeInMemory() {
+		memory = state;
+	}
+
+	public void clearMemory() {
+		memory = 0;
+	}
+
 	public void add(int value){
-		state += value;
+		try {
+			state += value;
+		} catch (Exception e) {
+			state = 0;
+		}
 	}
 
 	public void mult(int value){
-		state *= value;
+		try {
+			state *= value;
+		} catch (Exception e) {
+			state = 0;
+		}
 	}
 
 	public void sub(int value){
-		state -= value;
+		try {
+			state -= value;
+		} catch (Exception e) {
+			state = 0;
+		}
 	}
 
 	public void div(int value){
-		state /= value;
+		try {
+			state /= value;
+		} catch (Exception e) {
+			state = 0;
+		}
+	}
+
+	public void addFromMemory() {
+		add(memory);
+	}
+
+	public void subFromMemory() {
+		sub(memory);
+	}
+
+	public void divFromMemory() {
+		div(memory);
+	}
+
+	public void multFromMemory() {
+		mult(memory);
 	}
 
 }
--- src/test/java/kalkulator/CalculatorTest.java
+++ src/test/java/kalkulator/CalculatorTest.java
@@ -19,75 +19,197 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void testAddMinusOne(){
-		// Arrange
-		// sut = System Under Test
+	public void testAddNegativeNumbers(){
 		Calculator sut = new Calculator();
-		// Act
+		sut.setState(5);
 		sut.add(-1);
-		// Assert
-		assertEquals("0-1 = 1", -1, sut.getState());
+		assertEquals("5 + (-1) = 4", 4, sut.getState());
+		sut.add(-10);
+		assertEquals("4 + (-10) = -6", -6, sut.getState());
 	}
 
 	@Test
-	public void testMultOneByTwo(){
+	public void testAddToZero(){
+		Calculator sut = new Calculator();
+		sut.setState(0);
+		sut.add(100);
+		assertEquals("0+100 = 100", 100, sut.getState());
+	}
+
+	@Test
+	public void testIntegerRangeExceedAdd(){
+		Calculator sut = new Calculator();
+		sut.setState(2147483647);
+		sut.add(1);
+		assertEquals("2147483647 + 1 = 0",0, 0);
+	}
+
+	@Test
+	public void testIntegerRangeExceedMult(){
+		Calculator sut = new Calculator();
+		sut.setState(2147483647);
+		sut.add(2);
+		assertEquals("2147483647 * 2 = 0",0, 0);
+	}
+
+
+	@Test
+	public void testMultPositiveNumbers(){
 		Calculator sut = new Calculator();
 		sut.setState(1);
 		sut.mult(2);
 		assertEquals("1*2 = 2", 2, sut.getState());
+		sut.mult(3);
+		assertEquals("2*3 = 6", 6, sut.getState());
 	}
 
 	@Test
-	public void testMultOneByZero(){
+	public void testMultiNegativeNumbers(){
 		Calculator sut = new Calculator();
 		sut.setState(1);
-		sut.mult(0);
-		assertEquals("1*0 = 0", 0, sut.getState());
+		sut.mult(-2);
+		assertEquals("1*(-2) = -2", -2, sut.getState());
+		sut.mult(-3);
+		assertEquals("(-2)*(-3) = 6", 6, sut.getState());
 	}
 
 	@Test
-	public void testSubZero(){
+	public void testSubPositiveNumbers(){
 		Calculator sut = new Calculator();
-		sut.setState(1);
-		sut.sub(0);
-		assertEquals("1-0 = 1", 1, sut.getState());
+		sut.setState(5);
+		sut.sub(1);
+		assertEquals("5 - 1 = 4", 4, sut.getState());
+		sut.sub(10);
+		assertEquals("4 - 10 = -6", -6, sut.getState());
 	}
 
 	@Test
-	public void testSubTwoFromFour(){
+	public void testSubNegativeNumbers(){
 		Calculator sut = new Calculator();
-		sut.setState(4);
-		sut.sub(2);
-		assertEquals("4-2 = 2", 2, sut.getState());
+		sut.setState(5);
+		sut.sub(-1);
+		assertEquals("5 - (-1) = 6", 6, sut.getState());
+		sut.sub(-10);
+		assertEquals("6 - (-10) = 16", 16, sut.getState());
 	}
 
 	@Test
-	public void testSubTwoFromZero(){
+	public void testSubFromZero(){
 		Calculator sut = new Calculator();
-		sut.sub(2);
-		assertEquals("0-2 = -2", -2, sut.getState());
+		sut.setState(0);
+		sut.sub(100);
+		assertEquals("0 - 100 = -100", -100, sut.getState());
 	}
 
 	@Test
-	public void testDivByTwo(){
+	public void testIntegerRangeExceedSub(){
+		Calculator sut = new Calculator();
+		sut.setState(-2147483647);
+		sut.sub(1);
+		assertEquals("-2147483647 - 1 = 0",0, 0);
+	}
+	@Test
+	public void testDivByPositiveNumbers(){
 		Calculator sut = new Calculator();
 		sut.setState(6);
 		sut.div(2);
 		assertEquals("6/2 = 3", 3, sut.getState());
+		sut.div(3);
+		assertEquals("3/3 = 1", 1, sut.getState());
 	}
 
 	@Test
-	public void testZeroByTwo(){
+	public void testDivByNegativeNumbers(){
+		Calculator sut = new Calculator();
+		sut.setState(6);
+		sut.div(-2);
+		assertEquals("6/-2 = -3", -3, sut.getState());
+		sut.div(-3);
+		assertEquals("-3/-3 = -1", 1, sut.getState());
+	}
+
+	@Test
+	public void testZeroByNumber(){
 		Calculator sut = new Calculator();
 		sut.div(2);
 		assertEquals("0/2 = 0", 0, sut.getState());
+		sut.div(-3);
+		assertEquals("0/-3 = 0", 0, sut.getState());
 	}
 
 	@Test
-	public void DivByZero(){
+	public void testDivByZero(){
 		Calculator sut = new Calculator();
 		sut.setState(2);
 		sut.div(0);
+		assertEquals("2/0 = 2", 0, sut.getState());
+	}
+
+	@Test
+	public void testStoreInMemory(){
+		Calculator sut = new Calculator();
+		sut.setState(4);
+		sut.storeInMemory();
+		assertEquals(4, sut.getMemory());
+	}
+
+	@Test
+	public void testStoreInMemoryAfterOperations(){
+		Calculator sut = new Calculator();
+		sut.setState(4);
+		sut.add(2);
+		sut.storeInMemory();
+		assertEquals(6, sut.getMemory());
+	}
+
+	@Test
+	public void testAddFromMemory(){
+		Calculator sut = new Calculator();
+		sut.setState(4);
+		sut.storeInMemory();
+		sut.setState(2);
+		sut.addFromMemory();
+		assertEquals(6, sut.getState());
+	}
+
+	@Test
+	public void testSubFromMemory(){
+		Calculator sut = new Calculator();
+		sut.setState(2);
+		sut.storeInMemory();
+		sut.setState(6);
+		sut.subFromMemory();
+		assertEquals(4, sut.getState());
+	}
+
+	@Test
+	public void testDivFromMemory(){
+		Calculator sut = new Calculator();
+		sut.setState(2);
+		sut.storeInMemory();
+		sut.setState(6);
+		sut.divFromMemory();
+		assertEquals(3, sut.getState());
+	}
+
+	@Test
+	public void testMultFromMemory(){
+		Calculator sut = new Calculator();
+		sut.setState(2);
+		sut.storeInMemory();
+		sut.setState(6);
+		sut.multFromMemory();
+		assertEquals(12, sut.getState());
+	}
+
+	@Test
+	public void testClearMemoryMemory(){
+		Calculator sut = new Calculator();
+		sut.setState(2);
+		sut.storeInMemory();
+		sut.clearMemory();
+		assertEquals(0, sut.getMemory());
 	}
 }
+
 
